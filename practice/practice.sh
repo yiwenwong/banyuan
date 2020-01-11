a43642af3b21841a381b6c5b81d1caee2cfe9e18
banyuan <banyuan@wangyiwendeMacBook-Air.local>
2020-01-11 19:02:10 +0800

    first

---
+++ practice/practice.sh
@@ -0,0 +1,11 @@
+mkdir example
+mv ex01.c example/example.c
+mkdir practice
+mv ex02.c practice/ex02.c
+rm ex03.c
+touch README.md
+echo ##example/example.c  
+     判断是奇数还是偶数
+     ##practice/practice.c
+     比较两个数大小  >README.md
+     git push https://github.com/yiwenwong/banyuan.git  

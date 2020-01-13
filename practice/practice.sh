e179849ac738571005904f07b4ec836d6c8930a5
yiwenwong <58625966+yiwenwong@users.noreply.github.com>
2020-01-13 17:19:45 +0800

    Update practice.sh

--- practice/practice.sh
+++ practice/practice.sh
@@ -4,9 +4,9 @@ mkdir practice
 mv ex02.c practice/ex02.c
 rm ex03.c
 touch README.md
-echo ##example/example.c  
-     判断是奇数还是偶数
-     ##practice/practice.c
-     比较两个数大小  >README.md
+echo ##example/example.c  >README.md
+echo  判断是奇数还是偶数 >README.md
+echo  ##practice/practice.c>README.md
+echo   比较两个数大小  >README.md
      git add practice
      git commit -m"first"  
